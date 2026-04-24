ba4151c84b7ee714a7b112da10f3cdfe04e19d6a
barriosevelyn488-web <barrios.evelyn.488@gmail.com>
2026-04-24 09:50:46 -0600

    feat: :art: funciones de la 1 a la 4
    funciones de la 1 a la 4
    
    by Evelyn

--- ejercicios2.js
+++ ejercicios2.js
@@ -1,18 +1,57 @@
-
+//ejercicio1
 function funcionPar(){
-    var par = Number(prompt("ingrese su numero:"));
-    if (par > 0){
+    var par1 = Number(prompt("ingrese su numero:"));
+    if (par1 > 0){
         console.log("el numero es positivo");
-    } else if (par < 0){
+    } else if (par1 < 0){
         console.log("el numero es negativo");
     }  else {
         console.log("el numero es 0");
     }
 
-    return par;
+    return par1;
 }
 
 funcionPar();
+
+// ejercicio 2
+function funcionPar2(){
+    var num = Number(prompt("ingrese su numero"));
+    if (num % 2 == 0){
+        console.log("el numero es par");
+    } else if (num % 2 != 0 ){
+        console.log("el numero es impar");
+    } else {
+        console.log("numero invalido")
+    }
+
+    return num;
+
+}
+
+funcionPar2();
+
+
+//ejercicio 3
+function Mayoredad(){
+    var edad = Number(prompt("ingrese su edad"));
+    if (edad >= 18){
+        console.log("usted es mayor de edad");
+    } else if (edad < 18){
+        console.log("usted es menor de edad");
+    } else {
+        console.log("usted es un adulto mayor");
+    }
+
+    return edad;
+
+}
+
+Mayoredad();
+
+//ejercicio 4
+for 
+
 
 
 
